57a88c12009aedbea74a07a6c0da218dbcd1a030
JaesangKim01 <jeff010@g.skku.edu>
2025-04-09 10:36:00 +0000

    first commit

---
+++ arrange.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILES_DIR="./files"
+
+for file in "$FILES_DIR"/*; do
+    if [[ -f "$file" ]]; then
+        filename=$(basename "$file")
+        first_char=$(echo "${filename:0:1}" | tr '[:upper:]' '[:lower:]')
+        if [[ "$first_char" =~ [a-z] ]]; then
+            mv "$file" "./$first_char/"
+        fi
+    fi
+done
